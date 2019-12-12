0d1ad81ad8d53375b24386fb96d04e4771cedffc
fernandorubbo <fernando.rubbo@gmail.com>
2019-12-12 08:41:12 -0300

    cross platform build

--- 16_kubectl_plugin/install.sh
+++ 16_kubectl_plugin/install.sh
@@ -1,18 +1,29 @@
 for GOOS in darwin linux windows; do
   for GOARCH in 386 amd64; do
-    echo "Building $GOOS-$GOARCH"
-    export GOOS=$GOOS
-    export GOARCH=$GOARCH
-    go build -o bin/kubectl-snapshot-$GOOS-$GOARCH
+    output_name="bin/kubectl-snapshot-$GOOS-$GOARCH"
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+    echo "Building $output_name"
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name
+    if [ $? -ne 0 ]; then
+      echo 'An error has occurred! Aborting the script execution...'
+      exit 1
+    fi
   done
 done
 
+chmod +x ./sh/*
+cp ./sh/* ./bin/
+
 cd bin
+echo 'Tar gziping...'
 rm -rf kubectl-snapshot.tar.gz
 tar -cvzf kubectl-snapshot.tar.gz *
 cd ..
 
-go install
+cp bin/kubectl-snapshot.tar.gz ~/repos/github/fbrubbo/kubectl-plugins/
 cp bin/kubectl-snapshot-linux-amd64 ~/repos/github/fbrubbo/kubectl-plugins/kubectl-snapshot
-chmod +x ./sh/*
 cp ./sh/* ~/repos/github/fbrubbo/kubectl-plugins/ -f
+
+
