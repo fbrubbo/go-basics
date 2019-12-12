9ba88465f3624ef7bb2ed33c51cc3f1e39ac58a3
fernandorubbo <fernandorubbo@penguin>
2019-12-11 21:44:55 -0300

    cross platform build

--- 16_kubectl_plugin/install.sh
+++ 16_kubectl_plugin/install.sh
@@ -1,4 +1,18 @@
+for GOOS in darwin linux windows; do
+  for GOARCH in 386 amd64; do
+    echo "Building $GOOS-$GOARCH"
+    export GOOS=$GOOS
+    export GOARCH=$GOARCH
+    go build -o bin/kubectl-snapshot-$GOOS-$GOARCH
+  done
+done
+
+cd bin
+rm -rf kubectl-snapshot.tar.gz
+tar -cvzf kubectl-snapshot.tar.gz *
+cd ..
+
 go install
-cp ../../../../../bin//16_kubectl_plugin ~/repos/github/fbrubbo/kubectl-plugins/kubectl-snapshot
+cp bin/kubectl-snapshot-linux-amd64 ~/repos/github/fbrubbo/kubectl-plugins/kubectl-snapshot
 chmod +x ./sh/*
 cp ./sh/* ~/repos/github/fbrubbo/kubectl-plugins/ -f
